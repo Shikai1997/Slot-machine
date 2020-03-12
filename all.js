8ad5097e610c7356004154a1f8799c4d3265d09a
Shikai1997 <56082441+Shikai1997@users.noreply.github.com>
2020-03-12 23:52:52 +0800

    123

--- all.js
+++ all.js
@@ -1,38 +1,77 @@
 let star = document.querySelector('#star');
-let card_1 = document.querySelector('.card-1')
-let card_2 = document.querySelector('.card-2')
-let card_3 = document.querySelector('.card-3')
-star.addEventListener('click',run)
+let card_1 = document.querySelector('.card-1');
+let card_2 = document.querySelector('.card-2');
+let card_3 = document.querySelector('.card-3');
+star.addEventListener('click', run)
 
 
 
-//执行滚动   
-function run(){
+//執行  
+function run() {
 
     //初始位置
-    let times = 0;   
+    let times = 0;
 
-    //定时器   每20毫秒执行  看起来比较顺
-    setInterval(function(){
+    //定時器  20ms/次
+    let card1 = setInterval(function() {
         times += 10;
 
-        //每20毫秒  Y轴增加10px
-        card_1.style.transform = 'translateY(-'+times+'px)';
+        //20ms + Y軸10px
+        card_1.style.transform = 'translateY(-' + times + 'px)';
 
-        //归0
-        if(times === 1000){
+        //歸0
+        if (times === 1000) {
             card_1.style.transform = 'translateY(0px)';
             times = 0;
         }
-    },20)
+        console.log(times)
+    }, 30);
+
+    let card2 = setTimeout(function() {
+        setInterval(function() {
+            times += 10;
+
+            //20ms + Y軸10px
+            card_2.style.transform = 'translateY(-' + times + 'px)';
+
+            //歸0
+            if (times === 1000) {
+                card_2.style.transform = 'translateY(0px)';
+                times = 0;
+            }
+        }, 20);
+    }, 300)
+
+    let card3 = setTimeout(function() {
+        setInterval(function() {
+            times += 10;
+
+            //20ms + Y軸10px
+            card_3.style.transform = 'translateY(-' + times + 'px)';
+
+            //歸0
+            if (times === 1000) {
+                card_3.style.transform = 'translateY(0px)';
+                times = 0;
+            }
+        }, 10);
+    }, 600)
+
+
+
+    setTimeout(function() {
+        clearInterval(card1);
+    }, 8100)
+
 }
 
 
 
-//创造数字卡
-for(let i = 0; i<11;i++){
-    //后面补0  不然动画会有延迟
-    if(i === 10){
+
+//創造數字卡
+for (let i = 0; i < 11; i++) {
+    //後面補0  不然run時數字會斷掉
+    if (i === 10) {
         $('.card-1').append($('<li></li>').text(0).addClass('num'));
         $('.card-2').append($('<li></li>').text(0).addClass('num'));
         $('.card-3').append($('<li></li>').text(0).addClass('num'));
@@ -42,5 +81,3 @@ for(let i = 0; i<11;i++){
     $('.card-2').append($('<li></li>').text(i).addClass('num'));
     $('.card-3').append($('<li></li>').text(i).addClass('num'));
 }
-
-
