323109f2febbb7156290c6285a24df37bd4ee195
Shikai1997 <56082441+Shikai1997@users.noreply.github.com>
2020-03-14 02:08:56 +0800

    2020/03/14  完成

--- all.js
+++ all.js
@@ -3,69 +3,114 @@ let card_2 = document.querySelector(".card-2");
 let card_3 = document.querySelector(".card-3");
 let star = document.querySelector("#star");
 
+//結果
+let result1 = document.querySelector('.result1');
+let result2 = document.querySelector('.result2');
+let result3 = document.querySelector('.result3');
+
+//按鈕動畫
+let bottom = document.querySelector('.bottom');
+let center = document.querySelector('.center');
+
+//監聽
 star.addEventListener("click", run);
 
 //创造数字卡
 for (let i = 0; i < 11; i++) {
-  //后面补0  不然动画会有延迟
-  if (i === 10) {
-    $(".card-1").append($("<li></li>").text(0).addClass("num"));
-    $(".card-2").append($("<li></li>").text(0).addClass("num"));
-    $(".card-3").append($("<li></li>").text(0).addClass("num"));
-    break;
-  }
-  $(".card-1").append($("<li></li>").text(i).addClass("num"));
-  $(".card-2").append($("<li></li>").text(i).addClass("num"));
-  $(".card-3").append($("<li></li>").text(i).addClass("num"));
+    //後面補0   避免動畫延遲
+    if (i === 10) {
+        $(".card-1").append($("<li></li>").text(0).addClass("num"));
+        $(".card-2").append($("<li></li>").text(0).addClass("num"));
+        $(".card-3").append($("<li></li>").text(0).addClass("num"));
+        break;
+    }
+    $(".card-1").append($("<li></li>").text(i).addClass("num"));
+    $(".card-2").append($("<li></li>").text(i).addClass("num"));
+    $(".card-3").append($("<li></li>").text(i).addClass("num"));
 }
 
-//执行滚动
+//執行滾動
 function run() {
-  card1();
-  setTimeout(() => card2(), 300);
-  setTimeout(() => card3(), 600);
+    card1();
+    setTimeout(() => card2(), 300);
+    setTimeout(() => card3(), 600);
 
-  let ran1 = Math.floor( 5 + Math.random()*5);
-  let ran2 = Math.floor( 8 + Math.random()*8);
-  let ran3 = Math.floor( 11 + Math.random()*11);
+    animate();
 
 }
 
-//card-1定时器建立
+//按鈕動畫
+function animate() {
+    star.style.transform = 'translateY(20px)';
+    center.style.height = '20px';
+
+    //阻止點擊  下方2行
+    star.textContent = "wait";
+    star.classList.add('stop');
+
+    setTimeout(function() {
+        star.style.transform = 'translateY(0px)';
+        center.style.height = '40px';
+    }, 300);
+
+
+}
+
+//card-1定时器
 function card1() {
-  timer(0,card_1,30);
+    result1.style.display = 'none';
+    timer(0, card_1, 10); //變數3為速度
+    let num = Math.floor(Math.random() * 10);
+    result1.textContent = num;
+    setTimeout(() => clear(), 6000);
 }
 
-//card-2定时器建立
+//card-2定时器
 function card2() {
-  timer(0,card_2,20);
+    result2.style.display = 'none';
+    timer(0, card_2, 8); //變數3為速度
+    let num = Math.floor(Math.random() * 10);
+    result2.textContent = num;
+    setTimeout(() => clear(), 6000);
 }
 
-//card-3定时器建立
+//card-3定时器
 function card3() {
-  timer(0,card_3,10);
-
-  setTimeout(function(){
-      clearTimeout(timer);
-  },3500);
+    result3.style.display = 'none';
+    timer(0, card_3, 5); //變數3為速度
+    let num = Math.floor(Math.random() * 10);
+    result3.textContent = num;
+    setTimeout(() => clear(), 6000);
 }
 
 //定时器
-function timer(position,b,num) {
+function timer(position, b, num) {
 
-    setTimeout(function() {
-      position += 10;
+    settime = setTimeout(function() {
+        position += 10;
 
-      //每num毫秒  Y轴增加10px
-      b.style.transform = "translateY(-" + position + "px)";
+        //每num毫秒  Y轴增加10px
+        b.style.transform = "translateY(-" + position + "px)";
 
-      //归0
-      if (position === 1000) {
-       b.style.transform = "translateY(0px)";
-        position = 0;
-      }
-      timer(position,b,num);
+        //歸0
+        if (position === 1000) {
+            b.style.transform = "translateY(0px)";
+            position = 0;
+        }
+        timer(position, b, num);
     }, num);
-  };
 
+};
 
+//顯示結果 並清除定時器
+function clear() {
+    result1.style.display = 'flex';
+    result2.style.display = 'flex';
+    result3.style.display = 'flex';
+
+    //開放點擊
+    star.textContent = "star";
+    star.classList.remove('stop');
+
+    clearTimeout(settime)
+}
